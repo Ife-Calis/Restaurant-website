742b7536c54d3d0c20679409723567a743651a1b
ifechukwu <calistusifechukwu39@gmail.com>
2024-07-16 00:05:41 +0200

    Updated My Specialization Portfolio Project

--- src/data/data.js
+++ src/data/data.js
@@ -11,41 +11,41 @@ export const MenuList = [
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
     image: Dosa,
-    price: $25,
+    price: "$25"
   },
   {
     name: "Chola",
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
     image: Chola,
-    price: $40,
+    price: "$40"
   },
   {
     name: "Idli Sambhar",
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
     image: Idli,
-    price: $70,
+    price: "$70"
   },
   {
     name: "Masala Dosa",
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
     image: MasalaDosa,
-    price: $15,
+    price: "$15"
   },
   {
     name: "Paneer",
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
     image: Paneer,
-    price: $100,
+    price: "$100"
   },
   {
     name: "Gujrati",
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
     image: Gujrati,
-    price: $130,
+    price: "$130"
   },
 ];
